894cf23dd95dfca7b72d295263030b4bc74802bc
kaif <asifkaif21@gmail.com>
2024-05-16 13:57:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
-function App() {
+import { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+const App = () => {
+  const [textToCopy, setTextToCopy] = useState();
+
+  const clearText = () => {
+    setTextToCopy("");
+  };
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+  const { transcript, resetTranscript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(transcript);
+    setTextToCopy(transcript);
+  };
+
+  const handleClear = () => {
+    resetTranscript();
+    setTextToCopy("");
+    navigator.clipboard.writeText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="main-content">{transcript}</div>
+        <br></br>
+
+        <div className="btn-style">
+          <button onClick={handleCopy} className="button">
+            {textToCopy ? "Copied!" : "Copy to clipboard"}
+          </button>
+          <button onClick={startListening} className="button">
+            Start Listening
+          </button>
+          <button onClick={SpeechRecognition.stopListening} className="button">
+            Stop Listening
+          </button>
+          <button onClick={handleClear} className="button">
+            Clear Text
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
